1e61e4df3abe0aedd9765fb6febe1fa8caf0f518
BenjaminDaS <78204325+BenjaminDaS@users.noreply.github.com>
2023-03-13 15:46:14 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -8,11 +8,14 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
     int[] unicode = {0x1F60A, 0x1F601, 0x1F602, 0x1F643, 0x1F60D};
+    String[][] texte = {{"Adoration", "14/12/2022"}};
     TextView[] emoji;
+    TextView[] text1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text1 = new TextView[]{findViewById(R.id.text1), findViewById(R.id.text1Bis)};
         emoji = new TextView[]{findViewById(R.id.emoji1), findViewById(R.id.emoji2),
                 findViewById(R.id.emoji3), findViewById(R.id.emoji4), findViewById(R.id.emoji5)};
         emoji[0].setText(getEmojiByUnicode(unicode[0]));
@@ -20,6 +23,8 @@ public class MainActivity extends Activity {
         emoji[2].setText(getEmojiByUnicode(unicode[2]));
         emoji[3].setText(getEmojiByUnicode(unicode[3]));
         emoji[4].setText(getEmojiByUnicode(unicode[4]));
+        text1[0].setText(texte[0][0]);
+        text1[1].setText(texte[0][1]);
     }
 
     public String getEmojiByUnicode(int unicode){
