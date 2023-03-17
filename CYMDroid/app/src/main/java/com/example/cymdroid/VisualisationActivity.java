04a3ef12fd946b87e802b039a15b2a02ad6099bd
BenjaminDaS <benjiji3101@gmail.com>
2023-03-17 11:27:12 +0100

    configuration de la vue de visualisation des humeurs

--- CYMDroid/app/src/main/java/com/example/cymdroid/VisualisationActivity.java
+++ CYMDroid/app/src/main/java/com/example/cymdroid/VisualisationActivity.java
@@ -1,23 +1,30 @@
 package com.example.cymdroid;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
 
-public class VisualisationActivity extends Fragment implements View.OnClickListener {
+public class VisualisationActivity extends Fragment implements AdapterView.OnItemClickListener {
+    ListView liste;
+    List<ItemHumeur> humeurs;
 
-    public VisualisationActivity() {
-        // Required empty public constructor
-    }
 
     public static VisualisationActivity newInstance() {
         VisualisationActivity fragment = new VisualisationActivity();
         return fragment;
     }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,28 +32,39 @@ public class VisualisationActivity extends Fragment implements View.OnClickListe
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // On récupère la vue (le layout) associée au fragment un
-        View vueDuFragment = inflater.inflate(R.layout.fragment_un, container, false);
-        /*
-         * on associe un écouteur à chacun des 2 boutons de la vue : le fragment courant
-         * sera son propre écouteur de clic sur les boutons
-         */
-        vueDuFragment.findViewById(R.id.btn_alea).setOnClickListener(this);
-        vueDuFragment.findViewById(R.id.btn_vider).setOnClickListener(this);
-        // on récupère un accès au wiget qui affichera le nombre aléatoire
-        zoneResultat = vueDuFragment.findViewById(R.id.texte_resultat);
+                             Bundle savedInstanceState){
+        View vueDuFragment = inflater.inflate(R.layout.activity_visualisation, container, false);
+        liste = vueDuFragment.findViewById(R.id.liste);
+        humeurs = new ArrayList<>();
+        humeurs.add(new ItemHumeur("\uD83E\uDD13", "Adoration", "Ladate", "La description 1"));
+        humeurs.add(new ItemHumeur("\uD83E\uDD13", "Adoration", "Ladate", "La description 2"));
+        humeurs.add(new ItemHumeur("\uD83E\uDD13", "Adoration", "Ladate", "La description 3"));
+        humeurs.add(new ItemHumeur("\uD83E\uDD13", "Adoration", "Ladate", "La description 4"));
+        humeurs.add(new ItemHumeur("\uD83E\uDD13", "Adoration", "Ladate", "La description 5"));
+        ItemHumeurAdapter item = new ItemHumeurAdapter(this.getContext(), R.layout.vue_item_liste, humeurs);
+        liste.setAdapter(item);
+        liste.setOnItemClickListener(this);
         return vueDuFragment;
     }
 
+
     @Override
-    public void onClick(View v){
-        if (v.getId() == R.id.btn_vider) { // clic sur le bouton vider
-            zoneResultat.setText(getString(R.string.vide_alea));
-        } else { // clic sur le bouton générer
-            double nbAlea = Math.random();
-            int entierAlea = (int)(Math.random() * 1000);
-            zoneResultat.setText(getString(R.string.resultat_alea) + entierAlea);
-        }
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        onClickMood(i);
+    }
+
+    public void onClickMood(int i){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+
+        final View customLayout = getLayoutInflater().inflate(R.layout.alert_vue_description, null);
+        TextView titre = customLayout.findViewById(R.id.alertTitre);
+        titre.setText("Humeur");
+        TextView description = customLayout.findViewById(R.id.alertDescription);
+        description.setText(humeurs.get(i).getDescription());
+        builder.setView(customLayout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+        dialog.getWindow().setBackgroundDrawableResource(R.drawable.alerte_description_humeur);
     }
 }
