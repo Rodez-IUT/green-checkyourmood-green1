97a64b1098dda80dd628486537280f321283c069
Antoine Gouzy <103497638+LaG0uz@users.noreply.github.com>
2023-03-28 09:11:18 +0200

    Fin onglet ajout

--- CYMDroid/app/src/main/java/com/example/cymdroid/AjoutActivity.java
+++ CYMDroid/app/src/main/java/com/example/cymdroid/AjoutActivity.java
@@ -1,14 +1,32 @@
 package com.example.cymdroid;
 
+import android.app.AppComponentFactory;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 import androidx.fragment.app.Fragment;
 
 public class AjoutActivity extends Fragment implements View.OnClickListener {
+
+
+    final Calendar myCalendar= Calendar.getInstance();
+    EditText editText;
+    Spinner spinner;
 
     public AjoutActivity() {
         // Required empty public constructor
@@ -31,9 +49,56 @@ public class AjoutActivity extends Fragment implements View.OnClickListener {
         //On récupère la vue (le layout) associée au fragment un
         View vueDuFragment = inflater.inflate(R.layout.activity_ajout, container, false);
 
+        editText = vueDuFragment.findViewById(R.id.date);
+        spinner = vueDuFragment.findViewById(R.id.spinner);
+
+        List<String> humeurs = new ArrayList<String>();
+        humeurs.add(0, "Selectionner une humeur");
+        humeurs.add("Joie");
+        humeurs.add("Triste");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, humeurs);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (parent.getItemAtPosition(position).equals("Choose Football players from lis")){
+                }else {
+                    String item = parent.getItemAtPosition(position).toString();
+                    Toast.makeText(parent.getContext(),"Selected: " +item, Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AjoutActivity.newInstance().getContext(),date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
         return vueDuFragment;
     }
 
     @Override
     public void onClick(View v){ }
+
+    private void updateLabel(){
+        String myFormat="MM/dd/yy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.FRENCH);
+        editText.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
 }
