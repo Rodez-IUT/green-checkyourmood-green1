2e800295925ab24449e1be915be7b506db1c40d3
Courbaize <103497891+Courbaize@users.noreply.github.com>
2023-04-04 17:36:35 +0200

    fix try catch rollback

--- tests/services/MoodsServiceTest.php
+++ tests/services/MoodsServiceTest.php
@@ -83,134 +83,153 @@ class MoodsServiceTest extends TestCase {
     }
 
     public function testAddMood() {
-        $this->pdo->beginTransaction();
-        // when on essaye d'insérer un mood pour un utilisateur
-        date_default_timezone_set('Europe/Paris');
-        $dateHum = new DateTime();
-        $this->moodsService->addMood($this->pdo, 1, 15, $dateHum->format("Y-m-d H:i"), "");
-        $idHisto = $this->pdo->lastInsertId();
-        $result = $this->pdo->query("SELECT Code_Compte, Code_hum, Date_Hum, Informations
-                                   FROM historique
-                                   WHERE ID_Histo = $idHisto");
-        $result = $result->fetch();
-        // then le mood insérer est le dernier de l'utilisateur
-        $moodExpected = [
-            "Code_Compte" => 1,
-            "Code_hum" => 15,
-            "Date_Hum" => $dateHum->format("Y-m-d H:i:00"),
-            "Informations" => ""
-        ];
-        $this->assertEquals($moodExpected, $result);
-        $this->pdo->rollBack();
-        $this->pdo->beginTransaction();
         try {
-            // when on test d'ajouter une humeur invalide, then une erreur est levée
-            $this->moodsService->addMood($this->pdo, 1, 15, $dateHum->format("Y-m-d fzgzgzH:i"), "");
-        } catch( Exception) {
+            $this->pdo->beginTransaction();
+            // when on essaye d'insérer un mood pour un utilisateur
+            date_default_timezone_set('Europe/Paris');
+            $dateHum = new DateTime();
+            $this->moodsService->addMood($this->pdo, 1, 15, $dateHum->format("Y-m-d H:i"), "");
+            $idHisto = $this->pdo->lastInsertId();
+            $result = $this->pdo->query("SELECT Code_Compte, Code_hum, Date_Hum, Informations
+                                       FROM historique
+                                       WHERE ID_Histo = $idHisto");
+            $result = $result->fetch();
+            // then le mood insérer est le dernier de l'utilisateur
+            $moodExpected = [
+                "Code_Compte" => 1,
+                "Code_hum" => 15,
+                "Date_Hum" => $dateHum->format("Y-m-d H:i:00"),
+                "Informations" => ""
+            ];
+            $this->assertEquals($moodExpected, $result);
+            $this->pdo->rollBack();
+            $this->pdo->beginTransaction();
+            try {
+                // when on test d'ajouter une humeur invalide, then une erreur est levée
+                $this->moodsService->addMood($this->pdo, 1, 15, $dateHum->format("Y-m-d fzgzgzH:i"), "");
+            } catch( Exception) {
 
+            }
+            $this->pdo->rollBack();
+        } catch (Exception) {
+            $this->pdo->rollBack();
         }
-        $this->pdo->rollBack();
     }
 
     public function testEditMoodByIdHisto() {
-        $this->pdo->beginTransaction();
-        date_default_timezone_set('Europe/Paris');
-        $idHisto = 1;
-        // when on test de modifier une humeur
-        $this->moodsService->editMoodByIdHisto($this->pdo, $idHisto, 1, "2022-11-09 08:31", "Test");
-        $result = $this->pdo->query("SELECT ID_Histo, Code_Compte, Code_hum, Date_Hum, Informations
-                                   FROM historique
-                                   WHERE ID_Histo = $idHisto");
-        $result = $result->fetch();
-        $moodExpected = [
-            "ID_Histo" => $idHisto,
-            "Code_Compte" => 1,
-            "Code_hum" => 1,
-            "Date_Hum" => "2022-11-09 08:31:00",
-            "Informations" => "Test"
-        ];
-        // alors l'humeur est bien modifiée
-        $this->assertEquals($moodExpected, $result);
-
-        $this->pdo->rollBack();
-        $this->pdo->beginTransaction();
         try {
-            // when on teste de modifier une humeur avec une erreur de date, then une erreur est renvoyée
-            $this->moodsService->editMoodByIdHisto($this->pdo, $idHisto, 1, "2022-1seggzg1-09 08:31", "Test");
-        } catch( Exception) {
+            $this->pdo->beginTransaction();
+            date_default_timezone_set('Europe/Paris');
+            $idHisto = 1;
+            // when on test de modifier une humeur
+            $this->moodsService->editMoodByIdHisto($this->pdo, $idHisto, 1, "2022-11-09 08:31", "Test");
+            $result = $this->pdo->query("SELECT ID_Histo, Code_Compte, Code_hum, Date_Hum, Informations
+                                       FROM historique
+                                       WHERE ID_Histo = $idHisto");
+            $result = $result->fetch();
+            $moodExpected = [
+                "ID_Histo" => $idHisto,
+                "Code_Compte" => 1,
+                "Code_hum" => 1,
+                "Date_Hum" => "2022-11-09 08:31:00",
+                "Informations" => "Test"
+            ];
+            // alors l'humeur est bien modifiée
+            $this->assertEquals($moodExpected, $result);
 
+            $this->pdo->rollBack();
+            $this->pdo->beginTransaction();
+            try {
+                // when on teste de modifier une humeur avec une erreur de date, then une erreur est renvoyée
+                $this->moodsService->editMoodByIdHisto($this->pdo, $idHisto, 1, "2022-1seggzg1-09 08:31", "Test");
+            } catch( Exception) {
+
+            }
+            $this->pdo->rollBack();
+        } catch (Exception) {
+            $this->pdo->rollBack();
         }
-        $this->pdo->rollBack();
     }
 
     public function testDeleteMoodByIdHisto() {
-        $this->pdo->beginTransaction();
-        // when on teste de supprimer une humeur a une personne qui n'en a qu'une
-        $this->moodsService->deleteMoodByIdHisto($this->pdo, 1);
-        $verif = $this->pdo->query("SELECT COUNT(*) as nbRow FROM historique WHERE ID_Histo = 1");
-        $verif = $verif->fetch();
-        // Then il en a plus
-        $this->assertTrue($verif["nbRow"] == 0);
-        $this->pdo->rollBack();
+        try {
+            $this->pdo->beginTransaction();
+            // when on teste de supprimer une humeur a une personne qui n'en a qu'une
+            $this->moodsService->deleteMoodByIdHisto($this->pdo, 1);
+            $verif = $this->pdo->query("SELECT COUNT(*) as nbRow FROM historique WHERE ID_Histo = 1");
+            $verif = $verif->fetch();
+            // Then il en a plus
+            $this->assertTrue($verif["nbRow"] == 0);
+            $this->pdo->rollBack();
+        } catch (Exception) {
+            $this->pdo->rollBack();
+        }
     }
 
     public function testGetDiagramData() {
-        $this->pdo->beginTransaction();
-        // when on ajoute 4 humeurs à aujourd'hui
-        date_default_timezone_set('Europe/Paris');
-        $now = new DateTime();
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 14, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 4, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 4, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 7, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
-        // et on récupère les humeurs du jour grâce à la méthode getDiagram data
-        $result = $this->moodsService->getDiagramData($this->pdo, 2, "today");
-        // then on a les bonnes humeurs d'aujourd'hui
-        $resultExpected = [
-            ["Amusement", "Calme (sérénité)", "Envie (craving)"],
-            [4 => 2, 7 => 1, 14 => 1]
-        ];
-        $this->assertEquals($resultExpected, $result);
+        try {
+            $this->pdo->beginTransaction();
+            // when on ajoute 4 humeurs à aujourd'hui
+            date_default_timezone_set('Europe/Paris');
+            $now = new DateTime();
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 14, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 4, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 4, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 7, \"".$now->format("Y-m-d H:i")."\", \"".$now->format("Y-m-d H:i")."\", NULL)");
+            // et on récupère les humeurs du jour grâce à la méthode getDiagram data
+            $result = $this->moodsService->getDiagramData($this->pdo, 2, "today");
+            // then on a les bonnes humeurs d'aujourd'hui
+            $resultExpected = [
+                ["Amusement", "Calme (sérénité)", "Envie (craving)"],
+                [4 => 2, 7 => 1, 14 => 1]
+            ];
+            $this->assertEquals($resultExpected, $result);
 
-        // when on ajoute 4 humeurs à hier
-        $yesterday = $now;
-        $yesterday->modify("-1 day");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 5, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
-        $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
-                        VALUES(2, 1, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
-        // et on récupère les humeurs d'hier grâce à la méthode getDiagram data
-        $result = $this->moodsService->getDiagramData($this->pdo, 2, "last-day");
-        // then on a les bonnes humeurs d'hier
-        $resultExpected = [
-            ["Admiration", "Amusement", "Anxiété"],
-            [4 => 3, 5 => 1, 1 => 1]
-        ];
-        $this->assertEquals($resultExpected, $result);
-        $this->pdo->rollBack();
-        
+            // when on ajoute 4 humeurs à hier
+            $yesterday = $now;
+            $yesterday->modify("-1 day");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 5, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 4, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
+            $this->pdo->query("INSERT INTO historique(Code_Compte, Code_hum, Date_Hum, Date_Ajout, Informations)
+                            VALUES(2, 1, \"".$yesterday->format("Y-m-d H:i")."\", \"".$yesterday->format("Y-m-d H:i")."\", NULL)");
+            // et on récupère les humeurs d'hier grâce à la méthode getDiagram data
+            $result = $this->moodsService->getDiagramData($this->pdo, 2, "last-day");
+            // then on a les bonnes humeurs d'hier
+            $resultExpected = [
+                ["Admiration", "Amusement", "Anxiété"],
+                [4 => 3, 5 => 1, 1 => 1]
+            ];
+            $this->assertEquals($resultExpected, $result);
+            $this->pdo->rollBack();
+        } catch (Exception) {
+            $this->pdo->rollBack();
+        }
     }
 
     public function testGetCalenderData() {
-        $this->pdo->beginTransaction();
-        // when on récupère la data du calendrier par la méthode et par la base de données
-        $result = $this->moodsService->getCalenderData($this->pdo, 4, "2022-02", 28);
-        $stmt = $this->pdo->query("SELECT * FROM humeur WHERE ID_Hum = 11");
-        $humExpected = $stmt->fetch();
-        // then les deux sont égales
-        $this->assertEquals($humExpected, $result[4]);
+        try {
+            $this->pdo->beginTransaction();
+            // when on récupère la data du calendrier par la méthode et par la base de données
+            $result = $this->moodsService->getCalenderData($this->pdo, 4, "2022-02", 28);
+            $stmt = $this->pdo->query("SELECT * FROM humeur WHERE ID_Hum = 11");
+            $humExpected = $stmt->fetch();
+            // then les deux sont égales
+            $this->assertEquals($humExpected, $result[4]);
 
-        $this->pdo->rollBack();
+            $this->pdo->rollBack();
+        } catch (Exception) {
+            $this->pdo->rollBack();
+        }
     }
 
     public function testGetNbHum(){
